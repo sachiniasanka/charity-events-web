d6e9cc98f3d420c5a9e50be1c279cf5b67a36a0a
Darshana Kalubowila <kalubowilaksd@gmail.com>
2025-01-25 15:35:00 +0530

    profile pages

---
+++ src/app/(profile)/layout.jsx
@@ -0,0 +1,18 @@
+import "@/app/globals.css";
+import Navbar from "@/components/ui/Navbar";
+import Footer from "@/components/ui/Footer";
+
+export const metadata = {
+    title: "GIVEWELL",
+    description: "Be the Change You Wish to See",
+};
+
+export default function ProfileLayout({ children }) {
+    return (
+        <>
+            <Navbar isUserLoggedIn={true} />
+            {children}
+            <Footer />
+        </>
+    );
+}
